0bd922f77baad6d6a58f65c59a9c74b30fe785f6
daphne Fang <daphneys.fang@gmail.com>
2020-05-02 17:42:04 -0700

    updated databaseUrl

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgres://ydczvbazyjnjtc:8a6472260e0b46ca9b4fe4915f9359e816b8eb4351e865bef754e228dbb5b071@ec2-18-215-99-63.compute-1.amazonaws.com:5432/d8ha9lbnnokpjk',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://daphnefang@localhost/bookmarks',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://daphnefang@localhost/bookmarks-test'
 }
